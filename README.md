ec5fcea1749d331e8f20cd799e88dd770ed115df
charlocharlie <charlielaabs@gmail.com>
2024-03-31 19:29:43 -0500

    Add results to readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# top-games-size
+
+## Results
+
+Total storage size  of the top 100 rated games with 4 or more critic reviews on IGDB by platform:
+
+```txt
+Sony - PlayStation: 43.5 GiB
+Sony - PlayStation 2: 338.5 GiB
+Sony - PlayStation Portable: 88.0 GiB
+Nintendo - GameCube: 100.6 GiB
+Nintendo - Wii: 444.8 GiB
+Sega - Saturn: 1.9 GiB
+Sega - Dreamcast: 16.7 GiB
+Microsoft - Xbox: 204.1 GiB
+Microsoft - Xbox 360: 537.0 GiB
+```
--- top_games_size/__main__.py
+++ top_games_size/__main__.py
@@ -3,11 +3,11 @@ from top_games_size.igdb import get_platforms
 from top_games_size.platform import Platform
 
 platforms = [
-    Platform("Sony - PlayStation 2", 8),
     Platform("Sony - PlayStation", 7),
+    Platform("Sony - PlayStation 2", 8),
     Platform("Sony - PlayStation Portable", 38),
-    Platform("Nintendo - Wii", 5),
     Platform("Nintendo - GameCube", 21),
+    Platform("Nintendo - Wii", 5),
     Platform("Sega - Saturn", 32),
     Platform("Sega - Dreamcast", 23),
     Platform("Microsoft - Xbox", 11),
